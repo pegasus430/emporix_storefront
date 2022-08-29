ba6f470dc17e5ca22245dcc6481363fb4ce00613
smw <codility22@gmail.com>
2022-08-29 21:35:03 +0200

    adding products element

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Topbar from "./components/Header/topbar";
 import About from "./components/About/about"
 import Service from "./components/Service/Service";
+import Products from "./components/products/Products";
 function App() {
   return (
       <>
         <Topbar />
         <About />
         <Service />
+        <Products />
       </>
       
     
---
+++ src/components/products/Products.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import landing_bg from '../../assets/landing_bg.png'
+import intro from '../../assets/intro.png'
+import desk from '../../assets/products/desk.png'
+import seating from '../../assets/products/seating.png'
+import storage from '../../assets/products/storage.png'
+import printer from '../../assets/products/printer.png'
+import { PrinterIcon } from '@heroicons/react/solid'
+const EachProduct = (props) => {
+    return (
+        <div>
+            <img src = {props.src} className="w-full h-[240px]" />
+            <div className='pl-6 pt-6 font-inter font-semibold text-[20px] leading-[32px]'>
+                {props.title}
+            </div>
+            <div className="px-6 pt-[10px] font-normal text-base">
+                {props.content}
+            </div>
+        </div>
+    )
+}
+
+const Products = () => {
+
+  	return (
+    	<div className = " bg-gradient-to-b from-[#5F8FAA] to-[#87ABBF] w-full h-[1752px] md:h-[682px]  text-white">
+			<div className='hidden md:block md:pt-24 font-inter font-bold text-2xl text-center'>
+                Explore our products
+            </div>
+            <div className='hidden md:block md:pt-4 font-inter font-normal text-[16px] leading-[32px] text-center'>
+                Browse our catalogue and find the right product for you
+            </div>
+            <div className='px-6 py-12 md:pt-8 md:py-24 grid grid-cols-1 md:grid-cols-4 md:gap-12 gap-y-16 md:px-16' >
+                <EachProduct src = {seating} title="Seating" content = "Tellus ornare at consequat ipsum,non lobortis" />
+                <EachProduct src = {desk} title="Desk and Workspaces" content = "Tellus ornare at consequat ipsum,non lobortis" />
+                <EachProduct src = {storage} title="Storage" content = "Tellus ornare at consequat ipsum,non lobortis" />
+                <EachProduct src = {printer} title="Printers, Ink and Toner" content = "Tellus ornare at consequat ipsum,non lobortis" />
+            </div>
+
+    	</div>
+    
+  	)
+}
+
+export default Products
