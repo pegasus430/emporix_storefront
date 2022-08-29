4228c083d1b5caf65bdcd1401d8b6d1da6d89c5a
smw <codility22@gmail.com>
2022-08-29 12:48:10 +0200

    adding service element

---
+++ src/components/Service/Service.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const Service = () => {
+  
+
+  return (
+    <div className = "bg-[#87ABBF] w-full h-[470px] md:h-[304px] -mt-[1px] md:mt-[-28px] ">
+
+    </div>
+    
+  )
+}
+
+export default Service
