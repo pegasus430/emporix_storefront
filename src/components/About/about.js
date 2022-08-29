de756891e3a48f587cfbadcb3e9836ae28ab4d13
smw <codility22@gmail.com>
2022-08-29 12:48:02 +0200

    adding about element

---
+++ src/components/About/about.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import landing_bg from '../../assets/landing_bg.png'
+
+const About = () => {
+  
+
+  return (
+    <div style={{backgroundImage : `url(${landing_bg})`  }} className = "bg-[length:100%_150%] pt-20 md:pt-0 w-full h-[513px] md:h-[988px] ">
+
+    </div>
+    
+  )
+}
+
+export default About
