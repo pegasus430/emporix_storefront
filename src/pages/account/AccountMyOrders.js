4ac159664d6217bcd59babf629357c952af60c8c
Ensar <goldtrump1015@gmail.com>
2022-09-07 15:12:37 +0200

    Added account components.

---
+++ src/pages/account/AccountMyOrders.js
@@ -0,0 +1,6 @@
+const AccountMyOrders = () => {
+    return <h1>AccountMyOrders</h1>;
+};
+  
+export default AccountMyOrders;
+  
--- src/pages/account/AccountPage.js
+++ src/pages/account/AccountPage.js
@@ -1,12 +1,15 @@
 import React, { useState }  from 'react'
-import { Link }             from 'react-router-dom'
+import { Link, Outlet, useParams} from 'react-router-dom'
 
 import './account.css'
 
 const AccountPage = () => {
     return (
-        <>
-        </>
+        <div className="page-wrapper">
+            <div className="page-content">
+                <Outlet />
+            </div>
+        </div>
     )
 }
 
