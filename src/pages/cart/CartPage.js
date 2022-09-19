937e01cac388edf42df53141115aa3294f00e4b8
Ensar <goldtrump1015@gmail.com>
2022-09-19 08:50:20 +0200

    Added Cart Page

---
+++ src/pages/cart/CartPage.js
@@ -0,0 +1,38 @@
+import React, { useState }  from 'react'
+import './cart.css'
+import CartActionBar from './CartActionBar'
+import CartTable from './CartTable'
+import CartMobileContent from './CartMobileContent'
+import {CartActionPanel} from '../../components/Cart/cart'
+
+const CartPage = () => {
+    let CartProductList = localStorage.getItem('CartProductList')
+
+    CartProductList = CartProductList == null? {}: JSON.parse(CartProductList)
+
+    const products = Object.values(CartProductList)
+    const subtotalWithoutVat = products.map(product => product.price * product.buy_count).reduce((a,b)=> a + b)
+    
+    return (
+        <div className="cart-page-wrapper ">
+            <div className="cart-page-content">
+                <CartActionBar view={true}/>
+                <div className="lg:block hidden">
+                    <CartTable products={products}/>
+                </div>
+
+                <div className="lg:hidden">
+                    <CartMobileContent products={products}/>
+                </div>
+    
+                <div className="float-right">
+                    <div className="cart-action-panel-wrapper ml-auto">
+                        <CartActionPanel subtotalWithoutVat={subtotalWithoutVat} />
+                    </div>
+                </div>   
+                <CartActionBar />
+            </div>
+        </div>
+    )
+}
+export default CartPage
