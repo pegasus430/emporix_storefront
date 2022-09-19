66fd1f4019d5559684f46cd6e2360bc09a1d6c08
Ensar <goldtrump1015@gmail.com>
2022-09-19 14:05:09 +0200

    Added Checkout ProgressBar

--- src/pages/checkout/CheckoutActionPanel.js
+++ src/pages/checkout/CheckoutActionPanel.js
@@ -1,10 +1,11 @@
 import React, { useState }  from 'react'
+import {CartActionPanel} from '../../c'
 import './checkout.css'
 
 const CheckoutActionPanel = () => {
     return (
         <div className="checkout-action-panel-wrapper">
-            asdfasdf
+            
         </div>
     )
 }
