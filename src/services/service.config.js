51d81c443738e5a7de823a83e2c79bc7bb908af5
Ensar <goldtrump1015@gmail.com>
2022-09-23 00:44:11 +0200

    Call ServiceToken

--- src/services/service.config.js
+++ src/services/service.config.js
@@ -1,8 +1,8 @@
 const API_URL = process.env.REACT_APP_API_URL_STAGE
 const tenant = process.env.REACT_APP_TENANT_STAGE
-const client_id = process.env.REACT_APP_EMPORIX_SECRET_STAGE
-const client_secret = process.env.REACT_APP_EMPORIX_SECRET_STAGE
-const grant_type = "client_credentials"
+export const client_id = process.env.REACT_APP_EMPORIX_CLIENT_ID_STAGE
+export const client_secret = process.env.REACT_APP_EMPORIX_SECRET_STAGE
+export const grant_type = "client_credentials"
 
 export const service_token_api = `${API_URL}/oauth/token`
 export const category_api = `${API_URL}/category/${tenant}/category-trees`
