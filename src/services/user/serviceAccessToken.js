707b470393b43224400e74018f449ea6db3caf9b
Ensar <goldtrump1015@gmail.com>
2022-09-23 00:42:57 +0200

    Added Service Token

--- src/services/user/serviceAccessToken.js
+++ src/services/user/serviceAccessToken.js
@@ -1,9 +1,27 @@
-import service_token_api from '../service.config'
+import {service_token_api, grant_type, client_secret, client_id} from '../service.config'
 import ApiRequest  from '..'
+import {service_token_key} from '../../constants/localstorage'
+import qs from 'qs';
 
 const scope = "product.product_create product.product_publish category.category_create category.category_publish category.category_update saasag.brand_manage product.product_update product.product_publish product.product_delete product.product_delete_all"
 
-const ServiceAccessToken = () => {
-    
+const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
 }
+const payload = {
+    'grant_type': grant_type,
+    'client_id': client_id,
+    'client_secret': client_secret,
+    'scope': scope
+}
+const GetServiceTokenFromServer = async () => {
+    const res = await ApiRequest(service_token_api, 'post', qs.stringify(payload), headers)
+    localStorage.setItem(service_token_key, res['data']['access_token'])
+
+}
+const ServiceAccessToken = () => {
+    GetServiceTokenFromServer()
+    console.log('aaa')
+}
+
 export default ServiceAccessToken
