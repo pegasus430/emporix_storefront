37f5430b58ca5c166e0a0a5465ec577501c01612
smw <codility22@gmail.com>
2022-09-23 16:29:29 +0200

    adding classname in typograpghy

--- src/components/Utilities/typography/index.js
+++ src/components/Utilities/typography/index.js
@@ -1,77 +1,83 @@
 import './typography.css'
 
-export const Heading3 = ({children}) => {
+export const Heading2 = ({children, className}) => {
     return (
-        <div className="heading3">{children}</div>
+        <div className= { "heading2 " + (className? className: "")}>{children}</div>
     )
 }
-export const Heading4 = ({children}) => {
+export const Heading3 = ({children, className}) => {
     return (
-        <div className="heading4">{children}</div>
+        <div className= { "heading3 " + (className? className: "")}>{children}</div>
     )
 }
-export const Heading5 = ({children}) => {
+
+export const Heading4 = ({children, className}) => {
     return (
-        <div className="heading5">{children}</div>
+        <div className= { "heading4 " + (className? className: "")}>{children}</div>
     )
 }
-export const TextRegular1 = ({children}) => {
+export const Heading5 = ({children, className}) => {
     return (
-        <div className="text-regular-1">{children}</div>
+        <div className= { "heading5 " + (className? className: "")}>{children}</div>
     )
 }
-export const TextRegular = ({children}) => {
+export const TextRegular1 = ({children, className}) => {
     return (
-        <div className="text-regular">{children}</div>
+        <div className= { "text-regular-1 " + (className? className: "")}>{children}</div>
     )
 }
-export const TextBold2 = ({children}) => {
+export const TextRegular = ({children, className}) => {
     return (
-        <div className="text-bold-2">{children}</div>
+        <div className= { "text-regular " + (className? className: "")}>{children}</div>
     )
 }
-export const TextBold3 = ({children}) => {
+export const TextBold2 = ({children, className}) => {
     return (
-        <div className="text-bold-3">{children}</div>
+        <div className={"text-bold-2 " + (className? className: "")}>{children}</div>
     )
 }
-export const TextBold4 = ({children}) => {
+export const TextBold3 = ({children, className}) => {
     return (
-        <div className="text-bold-4">{children}</div>
+        <div className={"}text-bold-3 " + (className? className: "")}>{children}</div>
     )
 }
-export const TextBold5 = ({children}) => {
+export const TextBold4 = ({children, className}) => {
     return (
-        <div className="text-bold-5">{children}</div>
+        <div className={"text-bold-4 " + (className? className: "")}>{children}</div>
     )
 }
-export const TextBold6 = ({children}) => {
+export const TextBold5 = ({children}, className) => {
     return (
-        <div className="text-bold-6">{children}</div>
+        <div className= {"text-bold-5 " + (className? className: "")}>{children}</div>
     )
 }
-export const TextRegular2 = ({children}) => {
+export const TextBold6 = ({children, className}) => {
     return (
-        <div className="text-regular-2">{children}</div>
+        <div className={"text-bold-6 " + (className? className: "")}>{children}</div>
     )
 }
-export const TextRegular4 = ({children}) => {
+export const TextRegular2 = ({children, className}) => {
     return (
-        <div className="text-regular-4">{children}</div>
+        <div className={"text-regular-2 " + (className? className: "")}>{children}</div>
     )
 }
-export const TextRegular3 = ({children}) => {
+export const TextRegular4 = ({children, className}) => {
     return (
-        <div className="text-regular-4">{children}</div>
+        <div className= {"text-regular-4 " + (className? className: "")}>{children}</div>
     )
 }
-export const TextBold1 = ({children}) => {
+export const TextRegular3 = ({children, className}) => {
     return (
-        <div className="text-bold-1">{children}</div>
+        <div className= {"text-regular-4 " + (className? className: "")}>{children}</div>
     )
 }
-export const Underline = ({children}) => {
+export const TextBold1 = ({children, className}) => {
     return (
-        <div className="underline-normal-text">{children}</div>
+        <div className= {"text-bold-1 " + (className? className: "")}>{children}</div>
+    )
+}
+export const Underline = ({children, className}) => {
+    return (
+        <div className= {"underline-normal-text " + (className? className: "")}>{children}</div>
     )
 }
