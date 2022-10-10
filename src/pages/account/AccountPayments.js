d7941c1880ead3567e55a1204191007009a8424f
Ensar <goldtrump1015@gmail.com>
2022-10-10 23:25:30 +0200

    Added Credit Card Logon on Payment Method Page

--- src/pages/account/AccountPayments.js
+++ src/pages/account/AccountPayments.js
@@ -1,19 +1,25 @@
 import React, { useState }  from 'react'
 import { MediumPrimaryButton, MediumSecondaryButton} from '../../components/Utilities/button';
-import { Container, GridLayout } from '../../components/Utilities/common';
+import { Container, GridLayout, LayoutBetween } from '../../components/Utilities/common';
 import AccountLayout from './AccountLayout'
 import Switch from '@mui/material/Switch'
 import { Link } from 'react-router-dom';
+import masterCardLogo from '../../assets/mastercard.svg'
 import { TextBold2, TextBold7, Underline, TextRegular6, TextBold8, TextBold9} from '../../components/Utilities/typography';
 import './account.css'
-const PayItem = ({title, itemKey, className, children}) => {
+const PayItem = ({title, itemKey, className, children, logo}) => {
   const [active, setActive] = useState(false)
   return (
     <GridLayout className={"p-6 gap-4 border border-[#DFE1E5] " + (active?"bg-[#F5F8FA] border-[#214559]":"") }>
-      <Container className="gap-6 items-center"> 
-        <Switch className="switch-item" onClick={()=> setActive(!active)}/>
-        <TextBold2 className="text-[#214559]">{title}</TextBold2>
-      </Container>
+      <LayoutBetween>
+        <Container className="gap-6 items-center"> 
+          <Switch className="switch-item" onClick={()=> setActive(!active)}/>
+          <TextBold2 className="text-[#214559]">{title}</TextBold2>
+        </Container>
+        {logo!==undefined?<img src={logo} className="w-[60px]"/>:<></>
+        }
+      </LayoutBetween>
+        
       {children}
     </GridLayout>
   )
@@ -22,12 +28,14 @@ const Payments = () => {
   
   return (
     <GridLayout className="mt-20 gap-20">
-      <GridLayout className="gap-6 p-2 md:w-1/2 w-full m-auto">
-        <PayItem  title="Pay by Credit Card" itemKey="credit_card">
+      <GridLayout className="gap-6 p-2 xl:w-[55%] lg:w-[65%] w-full m-auto">
+        <PayItem  title="Pay by Credit Card" itemKey="credit_card" logo={masterCardLogo}>
             <TextBold7 className="text-[#818385]">Card Details</TextBold7>
             <Container>
               <TextRegular6 className="text-[#87ABBF] w-40">Card Number</TextRegular6>
               <TextBold8 className="text-[#377395]"> 1111 2222 3333 4444</TextBold8>
+              
+                
             </Container>
               
             <Container>
